8a39cfca09779034d0ce693e279c7b6240a74450
Jonathan Willianto <jowillianto@gmail.com>
2024-04-04 01:22:38 +0900

    added python wrappers (build by yourself)

--- python_wrapper/file_lock.cpp
+++ python_wrapper/file_lock.cpp
@@ -1,23 +1,25 @@
 #include <boost/python.hpp>
-#include <filesystem>
+#include <string>
 import file_lock;
 using namespace boost::python;
 
 BOOST_PYTHON_MODULE(file_lock){
-  class_<file_lock::BasicMutex, boost::noncopyable>("BasicMutex", init<std::filesystem::path>())
+  class_<file_lock::BasicMutex, boost::noncopyable>("BasicMutex", init<std::string>())
     .def("lock", &file_lock::BasicMutex::lock)
     .def("unlock", &file_lock::BasicMutex::unlock)
     .def("try_lock", &file_lock::BasicMutex::try_lock)
     .def("lock_shared", &file_lock::BasicMutex::lock_shared)
     .def("unlock_shared", &file_lock::BasicMutex::unlock_shared)
     .def("try_lock_shared", &file_lock::BasicMutex::try_lock_shared)
+    .def("protected_path_string", &file_lock::BasicMutex::protected_path_string)
   ;
-  class_<file_lock::LargeFileMutex, boost::noncopyable>("LargeFileMutex", init<std::filesystem::path>())
+  class_<file_lock::LargeFileMutex, boost::noncopyable>("LargeFileMutex", init<std::string>())
     .def("lock", &file_lock::LargeFileMutex::lock)
     .def("unlock", &file_lock::LargeFileMutex::unlock)
     .def("try_lock", &file_lock::LargeFileMutex::try_lock)
     .def("lock_shared", &file_lock::LargeFileMutex::lock_shared)
     .def("unlock_shared", &file_lock::LargeFileMutex::unlock_shared)
     .def("try_lock_shared", &file_lock::LargeFileMutex::try_lock_shared)
+    .def("protected_path_string", &file_lock::BasicMutex::protected_path_string)
   ;
 }
